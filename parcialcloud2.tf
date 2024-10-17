dcad6c595eab43e7714625b44237c2e5deb6b322
hvmiranda <91338828+hvmiranda@users.noreply.github.com>
2024-10-16 19:56:59 -0500

    Update parcialcloud2.tf

--- parcialcloud2.tf
+++ parcialcloud2.tf
@@ -26,7 +26,20 @@ resource "aws_internet_gateway" "parcial_igw" {
   }
 }
 
-resource "aws_route_table" "parcial_route_table" {
+resource "aws_route_table" "parcial_route_table_public" {
+  vpc_id = aws_vpc.parcial_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.parcial_igw.id
+  }
+
+  tags = {
+    Name = "parcial_route_table"
+  }
+}
+
+resource "aws_route_table" "parcial_route_table_private" {
   vpc_id = aws_vpc.parcial_vpc.id
 
   route {
@@ -63,19 +76,19 @@ resource "aws_subnet" "public_subnet_02" {
 
 resource "aws_route_table_association" "public_subnet_01_association" {
   subnet_id      = aws_subnet.public_subnet_01.id
-  route_table_id = aws_route_table.parcial_route_table.id
+  route_table_id = aws_route_table.parcial_route_table_public.id
 }
 
 resource "aws_route_table_association" "public_subnet_02_association" {
   subnet_id      = aws_subnet.public_subnet_02.id
-  route_table_id = aws_route_table.parcial_route_table.id
+  route_table_id = aws_route_table.parcial_route_table_public.id
 }
 
 resource "aws_subnet" "private_subnet_01" {
   vpc_id                  = aws_vpc.parcial_vpc.id
   cidr_block              = "70.0.3.0/24"
   map_public_ip_on_launch = false
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-west-2c"
 
   tags = {
     Name = "private_subnet_01"
@@ -86,9 +99,19 @@ resource "aws_subnet" "private_subnet_02" {
   vpc_id                  = aws_vpc.parcial_vpc.id
   cidr_block              = "70.0.4.0/24"
   map_public_ip_on_launch = false
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-west-2d"
 
   tags = {
     Name = "private_subnet_02"
   }
+}
+
+resource "aws_route_table_association" "private_subnet_01_association" {
+  subnet_id      = aws_subnet.private_subnet_01.id
+  route_table_id = aws_route_table.parcial_route_table_private.id
+}
+
+resource "aws_route_table_association" "private_subnet_02_association" {
+  subnet_id      = aws_subnet.private_subnet_02.id
+  route_table_id = aws_route_table.parcial_route_table_private.id
 }
